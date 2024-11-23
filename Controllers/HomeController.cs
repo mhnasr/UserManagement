ea83597f04b54b1d8bd7d73b100621261c416a4c
mhnasr <nasr302@yahoo.com>
2024-11-23 14:37:00 +0330

    بدون لاگین

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using UserManagement.Models;
 
 namespace UserManagement.Controllers
 {
-    [Authorize]
+   // [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
